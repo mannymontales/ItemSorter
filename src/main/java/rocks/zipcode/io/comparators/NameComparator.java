c3cb6b56c8c42b3365cd866a5eca9170f9084bc5
Manny <manny@Zipcoders-MacBook-Pro-58.local>
2023-07-26 17:20:47 -0400

    current working things,moving to pc

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,13 @@
 package rocks.zipcode.io.comparators;
 
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
 }
